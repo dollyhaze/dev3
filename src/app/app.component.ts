9ac34f39b67c8d392dc46700a6e3a156ff21bfb9
Mistermaru <mistermaru_@live.nl>
2017-04-13 22:38:20 +0200

    Added a map and a sidebar to the webapplication.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,82 +1,58 @@
-/*
- * Angular 2 decorators and services
- */
-import {
-  Component,
-  OnInit,
-  ViewEncapsulation
-} from '@angular/core';
-import { AppState } from './app.service';
+import { Component } from '@angular/core';
+import { SidebarModule } from 'ng-sidebar';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, ApplicationRef } from '@angular/core';
+import { SebmGoogleMap, SebmGoogleMapMarker } from 'angular2-google-maps/core';
 
-/*
- * App Component
- * Top Level Component
- */
+var height = window.innerHeight;
 @Component({
   selector: 'app',
-  encapsulation: ViewEncapsulation.None,
-  styleUrls: [
-    './app.component.css'
-  ],
+	styles: [ `
+		.sebm-google-map-container {
+			height: ${height}px;
+		}
+	` ],
   template: `
-    <nav>
-      <a [routerLink]=" ['./'] "
-        routerLinkActive="active" [routerLinkActiveOptions]= "{exact: true}">
-        Index
-      </a>
-      <a [routerLink]=" ['./home'] "
-        routerLinkActive="active" [routerLinkActiveOptions]= "{exact: true}">
-        Home
-      </a>
-      <a [routerLink]=" ['./detail'] "
-        routerLinkActive="active" [routerLinkActiveOptions]= "{exact: true}">
-        Detail
-      </a>
-      <a [routerLink]=" ['./barrel'] "
-        routerLinkActive="active" [routerLinkActiveOptions]= "{exact: true}">
-        Barrel
-      </a>
-      <a [routerLink]=" ['./about'] "
-        routerLinkActive="active" [routerLinkActiveOptions]= "{exact: true}">
-        About
-      </a>
-    </nav>
+    <ng-sidebar-container>
+      <ng-sidebar [(opened)]="opened">
+        <p>Sidebar contents</p>
+      </ng-sidebar>
+    <button (click)="toggleSidebar()">Toggle sidebar</button>
+    </ng-sidebar-container>
 
-    <main>
-      <router-outlet></router-outlet>
-    </main>
-
-    <pre class="app-state">this.appState.state = {{ appState.state | json }}</pre>
-
-    <footer>
-      <span>WebPack Angular 2 Starter by <a [href]="url">@AngularClass</a></span>
-      <div>
-        <a [href]="url">
-          <img [src]="angularclassLogo" width="25%">
-        </a>
-      </div>
-    </footer>
-  `
+    <sebm-google-map [latitude]="lat" [styles]="mapstyle" [longitude]="lng">
+      <sebm-google-map-marker [latitude]="markerlat" [longitude]="markerlng" [label]="'P'">
+      </sebm-google-map-marker>
+    </sebm-google-map>
+	`
 })
-export class AppComponent implements OnInit {
-  public angularclassLogo = 'assets/img/angularclass-avatar.png';
-  public name = 'Angular 2 Webpack Starter';
-  public url = 'https://twitter.com/AngularClass';
-
-  constructor(
-    public appState: AppState
-  ) {}
-
-  public ngOnInit() {
-    console.log('Initial App State', this.appState.state);
+export class AppComponent {
+  lat: number = 51.924420;
+  lng: number = 4.477733;
+  markerlat: number = 51.924420;
+  markerlng: number = 5.517733;
+  opened: boolean = false;
+  toggleSidebar() {
+    this.opened = !this.opened;
   }
-
+  directives: [SebmGoogleMap, SebmGoogleMapMarker];
+  mapstyle = [{
+    "stylers": [{
+        "hue": "#ff1a00"
+    }, {
+        "invert_lightness": true
+    }, {
+        "saturation": -100
+    }, {
+        "lightness": 33
+    }, {
+        "gamma": 0.5
+    }]
+}, {
+    "featureType": "water",
+    "elementType": "geometry",
+    "stylers": [{
+        "color": "#2D333C"
+    }]
+}]
 }
-
-/*
- * Please review the https://github.com/AngularClass/angular2-examples/ repo for
- * more angular app examples that you may copy/paste
- * (The examples may not be updated as quickly. Please open an issue on github for us to update it)
- * For help or questions please contact us at @AngularClass on twitter
- * or our chat on Slack at https://AngularClass.com/slack-join
- */
