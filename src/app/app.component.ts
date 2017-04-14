3abf0887a1563c7e2144473852a2b653782bbc4e
oscar <druiventakoscar@gmail.com>
2017-04-15 00:32:30 +0200

    update map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SidebarModule } from 'ng-sidebar';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, ApplicationRef } from '@angular/core';
-import { SebmGoogleMap, SebmGoogleMapMarker } from 'angular2-google-maps/core';
-
+import { SebmGoogleMap, SebmGoogleMapMarker, SebmGoogleMapInfoWindow } from 'angular2-google-maps/core';
+import 'rxjs/Rx';
+import {Http} from '@angular/http';
 var height = window.innerHeight;
 @Component({
   selector: 'app',
@@ -13,29 +14,46 @@ var height = window.innerHeight;
 		}
 	` ],
   template: `
-    <ng-sidebar-container>
-      <ng-sidebar [(opened)]="opened">
-        <p>Sidebar contents</p>
-      </ng-sidebar>
-    <button (click)="toggleSidebar()">Toggle sidebar</button>
-    </ng-sidebar-container>
-
-    <sebm-google-map [latitude]="lat" [styles]="mapstyle" [longitude]="lng">
-      <sebm-google-map-marker [latitude]="markerlat" [longitude]="markerlng" [label]="'P'">
+    <sebm-google-map [zoom]="zoomlvl" [latitude]="lat" [styles]="mapstyle" [longitude]="lng">
+      <sebm-google-map-marker
+      *ngFor="let event of events"
+      iconUrl="assets/img/marker-images/image.png"
+      [latitude]="convertToNumber(event.latitude)"
+      [longitude]="convertToNumber(event.longitude)"
+      [label]="'P'"
+      >
+      <sebm-google-map-info-window [disableAutoPan]="true">
+         {{event.title}}<br>
+         <b>meer info...</b>
+       </sebm-google-map-info-window>
       </sebm-google-map-marker>
     </sebm-google-map>
 	`
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   lat: number = 51.924420;
   lng: number = 4.477733;
+  zoomlvl: number = 14;
   markerlat: number = 51.924420;
   markerlng: number = 5.517733;
   opened: boolean = false;
-  toggleSidebar() {
-    this.opened = !this.opened;
+  events = [];
+  constructor(private http: Http) {}
+
+  ngOnInit() {
+    this.getEvents()
   }
-  directives: [SebmGoogleMap, SebmGoogleMapMarker];
+  private convertToNumber(value: string): number {
+    return +value
+  }
+  getEvents() {
+    this.http.get('http://localhost:4000/events')
+      .map(res => res.json())
+      .subscribe(event => {
+        this.events = event.events
+      }, console.log)
+  }
+  directives: [SebmGoogleMap, SebmGoogleMapMarker, SebmGoogleMapInfoWindow];
   mapstyle = [{
     "stylers": [{
         "hue": "#ff1a00"
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { SidebarModule } from 'ng-sidebar';
 import { NgModule, ApplicationRef } from '@angular/core';
 import { AgmCoreModule } from 'angular2-google-maps/core';
+import {HttpModule, JsonpModule} from '@angular/http'
 @NgModule({
   imports: [
+    HttpModule,
+    JsonpModule,
     BrowserModule,
     SidebarModule.forRoot(),
     AgmCoreModule.forRoot({apiKey: 'AIzaSyCY6otWv1cmM1xjbHFRFd03pWOCbwT-SFk '}),
